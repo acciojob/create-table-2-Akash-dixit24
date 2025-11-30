ba188a630d663098c01e5bd95c281b980252104c
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2025-11-30 09:01:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 function createTable() {
-    //Write your code here
-  
+    let rn = prompt("Input number of rows");
+    let cn = prompt("Input number of columns");
+
+    rn = Number(rn);
+    cn = Number(cn);
+
+    if (!Number.isInteger(rn) || !Number.isInteger(cn)) return;
+    if (rn <= 0 || cn <= 0) {
+        alert("Rows and columns must be positive numbers!");
+        return;
+    }
+
+    const table = document.getElementById("myTable");
+    table.innerHTML = "";
+
+    for (let i = 0; i < rn; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < cn; j++) {
+            const cell = document.createElement("td");
+            cell.textContent = `Row-${i} Column-${j}`;
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
 }
